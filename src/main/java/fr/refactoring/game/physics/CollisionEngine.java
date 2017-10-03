32788f73a672b82e852d620554b6b41e73ea0175
Moura De Oliveira Lucas <mouradeolive@b10p22.fil.univ-lille1.fr>
2017-10-03 15:08:45 +0200

    correction déplacement

--- src/main/java/fr/refactoring/game/physics/CollisionEngine.java
+++ src/main/java/fr/refactoring/game/physics/CollisionEngine.java
@@ -125,32 +125,6 @@ public class CollisionEngine implements ICollisionEngine {
 	    return null; // No collision
 	}
 	
-	// Returns 1 if the lines intersect, otherwise 0. In addition, if the lines 
-	// intersect the intersection point may be stored in the floats i_x and i_y.
-	/*char get_line_intersection(float p0_x, float p0_y, float p1_x, float p1_y, 
-	    float p2_x, float p2_y, float p3_x, float p3_y, float *i_x, float *i_y)
-	{
-	    float s1_x, s1_y, s2_x, s2_y;
-	    s1_x = p1_x - p0_x;     s1_y = p1_y - p0_y;
-	    s2_x = p3_x - p2_x;     s2_y = p3_y - p2_y;
-
-	    float s, t;
-	    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-	    t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-
-	    if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
-	    {
-	        // Collision detected
-	        if (i_x != NULL)
-	            *i_x = p0_x + (t * s1_x);
-	        if (i_y != NULL)
-	            *i_y = p0_y + (t * s1_y);
-	        return 1;
-	    }
-
-	    return 0; // No collision
-	}*/
-
 	private List<Line2D.Double> getLinesForMovement(PositionComponent position, SizeComponent size, VelocityComponent velocity) {
 		List<Line2D.Double> lines = new ArrayList<Line2D.Double>();
 		double width = size.getWidth()/2;
@@ -187,7 +161,7 @@ public class CollisionEngine implements ICollisionEngine {
 		for(int i=cellX1; i <= cellX2; i++) {
 			for (int j = cellY1; j <= cellY2; j++) {
 				if(!gameMap.isEmpty(gameMap.getTypeForCell(i, j))) {
-					hitboxes.add(new Rectangle2D.Double(i*cellSize, j*cellSize, cellSize, cellSize));
+					hitboxes.add(new Rectangle2D.Double(i*cellSize-1, j*cellSize-1, cellSize+2, cellSize+2));
 				}
 			}
 		}
--- src/main/java/fr/refactoring/game/system/PlayerMovementSystem.java
+++ src/main/java/fr/refactoring/game/system/PlayerMovementSystem.java
@@ -42,8 +42,10 @@ public class PlayerMovementSystem extends EntitySystem {
 	public void move(Entity entity){
 		VelocityComponent vc = Mapper.velocityMapper.get(entity);
 		
-		if(vc.getDx() == 0 && vc.getDy() == 0)
+		//Si pas de déplacement, on quitte
+		if(vc.isVoid()) {
 			return;
+		}
 		
 		PositionComponent pc = Mapper.positionMapper.get(entity);
 		SizeComponent sc = Mapper.sizeMapper.get(entity);
@@ -97,110 +99,5 @@ public class PlayerMovementSystem extends EntitySystem {
 			
 		}
 	}
-	
-	/*private void oldMove() {
-		int speed = mc.getSpeed();
-		List<Point> hitboxPoints = null;
-		int x = 0;
-		int y = 0;
-				
-		if(c[0]){ // north
-			x = 0;
-			y = -speed;
-			hitboxPoints = this.getHitboxForEntityAndDirection(entity, "NORTH");
-			calculerDeplacement("NORTH", x, y, hitboxPoints, entity);
-		}
-		if(c[1]){ //south
-			x = 0;
-			y = speed;
-			hitboxPoints = this.getHitboxForEntityAndDirection(entity, "SOUTH");
-			calculerDeplacement("SOUTH", x, y, hitboxPoints, entity);
-		}
-		if(c[2]){ // west
-			x = -speed;
-			y = 0;
-			hitboxPoints = this.getHitboxForEntityAndDirection(entity, "WEST");
-			calculerDeplacement("WEST", x, y, hitboxPoints, entity);
-		}
-		if(c[3]){ // east
-			x = speed;
-			y = 0;
-			hitboxPoints = this.getHitboxForEntityAndDirection(entity, "EAST");
-			calculerDeplacement("EAST", x, y, hitboxPoints, entity);
-		}
-			
-	}*/
-		
-	/*private void calculerDeplacement(String direction, int x, int y, List<Point> points, Entity entity) {
-		
-		Position mc = mm.get(entity);
-		TypeComponent tc = tm.get(entity);
-		
-		Point p1 = new Point(points.get(0).x+x,points.get(0).y+y);
-		Point p2 = new Point(points.get(1).x+x,points.get(1).y+y);
-		int typeP1 = this.gameMap.getTypeForPosition(p1);
-		int typeP2 = this.gameMap.getTypeForPosition(p2);
-		boolean emptyP1 = gameMap.isEmpty(typeP1);
-		boolean emptyP2 = gameMap.isEmpty(typeP2);
-		
-		if(emptyP1 && emptyP2){
-			mc.setX(mc.getX()+x);
-			mc.setY(mc.getY()+y);
-		}else{
-			if(direction.equals("NORTH")){
-				if(emptyP1){
-					mc.setY((p1.y/32+1)*32+1+mc.getSize()/2);				
-				}else{
-					mc.setY((p2.y/32+1)*32+1+mc.getSize()/2);		
-				}
-			}else if(direction.equals("SOUTH")){
-				if(emptyP1){
-					mc.setY(p1.y/32*32-mc.getSize()/2-1);				
-				}else{
-					mc.setY(p2.y/32*32-mc.getSize()/2-1);	
-				}
-			}else if(direction.equals("WEST")){
-				if(emptyP1){
-					mc.setX((p1.x/32+1)*32+1+mc.getSize()/2);
-				}else{
-					mc.setX((p2.x/32+1)*32+1+mc.getSize()/2);
-				}
-			}else if(direction.equals("EAST")){
-				if(emptyP1){
-					mc.setX(p1.x/32*32-mc.getSize()/2-1);
-				}else{
-					mc.setX(p2.x/32*32-mc.getSize()/2-1);
-				}
-			}
-		}
-		
-		//Si l'entité est un poulet et qu'il est sur un arbre, on le masque
-		if(tc.getType().equalsIgnoreCase("Poulet")) {
-			VisibilityComponent vc = vm.get(entity);
-			vc.setVisible(!gameMap.isTree(typeP1));
-		}
-	}
-	
-	private List<Point> getHitboxForEntityAndDirection(Entity entity, String direction) {
-		Position mc = mm.get(entity);
-		List<Point> points = new ArrayList<Point>();
-		int size = mc.getSize();
-		int x1 = mc.getX() - (size/2);
-		int y1 = mc.getY() - (size/2);
-		if(direction.equals("NORTH")){
-			points.add(new Point(x1,y1));
-			points.add(new Point(x1+size,y1));
-		}else if(direction.equals("SOUTH")){
-			points.add(new Point(x1,y1+size));
-			points.add(new Point(x1+size,y1+size));		
-		}else if(direction.equals("WEST")){
-			points.add(new Point(x1,y1));
-			points.add(new Point(x1,y1+size));
-		}else if(direction.equals("EAST")){
-			points.add(new Point(x1+size,y1));
-			points.add(new Point(x1+size,y1+size));
-		}
-		return points;
-	}*/
 
 }
